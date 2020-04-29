220237aefa0330451f0e0caa6be829891a90739a
Fernando Malveira Aragão <fernandoarag@gmail.com>
2020-04-29 14:29:26 -0300

    Add files via upload

---
+++ _incluir/rodape.php
@@ -0,0 +1,5 @@
+<footer>
+    <div id="footer_central">
+        <p>© Copyright 2020 - All Rights Reserved - Fernando Aragão</p>
+    </div>
+</footer>
---
+++ _incluir/topo.php
@@ -0,0 +1,5 @@
+<header>
+    <div id="header_central">
+
+    </div>
+</header>
