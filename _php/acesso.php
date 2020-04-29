081670ec9f3eab60e49c20b2769d7d4c83ac0153
Fernando Malveira Aragão <fernandoarag@gmail.com>
2020-04-29 16:28:11 -0300

    Update acesso.php

--- _php/acesso.php
+++ _php/acesso.php
@@ -1,6 +1,6 @@
 <?php 
     $filename = "answer.php";
-    $token = "e0098b7860b7ead2c664ed00a3385d94dc8b725e";
+    $token = "";
     $url_receber = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token={$token}";
     $url_enviar = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token={$token}";
 ?>
